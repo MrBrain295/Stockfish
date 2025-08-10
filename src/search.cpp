37fc854ca9e25e18fe1b6d25108d64cf4fcf62e5
Gabe <66077254+MrBrain295@users.noreply.github.com>
2025-08-10 14:15:49 -0500

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -1053,7 +1053,7 @@ moves_loop:  // When in check, search starts here
                 if (!pos.see_ge(move, -margin))
                 {
                     bool mayStalemateTrap =
-                      depth > 2 && alpha < 0 && pos.non_pawn_material(us) == PieceValue[movedPiece]
+                      depth > 3 && alpha < 0 && pos.non_pawn_material(us) == PieceValue[movedPiece]
                       && PieceValue[movedPiece] >= RookValue
                       // it can't be stalemate if we moved a piece adjacent to the king
                       && !(attacks_bb<KING>(pos.square<KING>(us)) & move.from_sq())
